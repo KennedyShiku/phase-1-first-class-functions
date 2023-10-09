09bedf807388c90b39e6d1c0c5dd8e09acfd95c0
KennedyShiku <kenrichshiku@gmail.com>
2023-10-09 04:04:10 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+const chai = require("chai");
+const spies = require("chai-spies");
+chai.use(spies);
+
+const expect = chai.expect;
+
+function receivesAFunction(callback) {
+  callback();
+}
+
+function returnsANamedFunction() {
+  return function namedFunction() {};
+}
+
+function returnsAnAnonymousFunction() {
+  return function() {};
+}
